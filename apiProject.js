ba07350243e3be2720697850e426f1c594c4870d
Peyton Nix <peytonnix50@gmail.com>
2020-09-17 21:27:12 -0500

    finished project

--- apiProject.js
+++ apiProject.js
@@ -1,3 +1,19 @@
-fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
-  .then(res => res.json())
-  .then(json => console.log(json))
+let pokemonList = document.querySelector('ul'); 
+
+fetch('https://pokeapi.co/api/v2/pokemon?limit=151') 
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        let pokemon = json.results;
+
+        for (p of pokemon){  
+            let listItem = document.createElement('li');
+            listItem.innerHTML = '<p>' + p.name + '<p>';
+            pokemonList.appendChild(listItem);
+            
+            let imgSrc = document.createElement('img');
+            imgSrc.src = `https://img.pokemondb.net/artwork/large/${p.name}.jpg`;
+            pokemonList.appendChild(imgSrc);
+        }})
+console.log(pokemonList);
